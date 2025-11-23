e47a5cf0c56fd2dfec38338fa9876a8a210fd44f
Abhijit mondal <46677857+abhijit543@users.noreply.github.com>
2025-11-23 20:21:12 +0530

    Add useCallback import to Carousel component

--- src/client/component/home/carousel/Carousel.jsx
+++ src/client/component/home/carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useCallback } from "react";
 import SwipeableViews from "react-swipeable-views";
 import { Typography, Box, Button } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
